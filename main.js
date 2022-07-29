88083f4af751e5c0785cd003c17b338d9be302e9
Saket B <ken.life213@gmail.com>
2022-07-29 13:08:35 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function link(e) {
+    let link
+    if(e==='one') {
+        link = "https://mr-mystery-13.github.io/hosting-demo/";
+        return link
+    }
+    if(e==="two") {
+        link = "https://stackoverflow.com/questions/15455289/changing-variable-by-html-button"
+        return link
+    }
+}
