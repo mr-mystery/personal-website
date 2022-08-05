69bd4c7a91ac0e4a877309318f83be95e1c17c23
Saket B <ken.life213@gmail.com>
2022-08-05 13:37:00 -0700

    Fix

--- Home/main.js
+++ Home/main.js
@@ -1,4 +1,5 @@
 
+
 function link(num) {
     let iframe
     if(num==='one') {
@@ -10,3 +11,17 @@ function link(num) {
         iframe.setAttribute('src', 'https://mr-mystery-13.github.io/Personal-Website/')
     }
 }
+
+
+me = {
+    L1: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, animi repellat! Saepe recusandae numquam eligendi praesentium dolorum cumque minima ipsum, obcaecati, a molestiae at, excepturi reiciendis quisquam. Modi, quisquam ipsam?",
+    L2: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime ea eius corporis animi rem eveniet nostrum temporibus facilis illum unde quia doloremque, sit itaque molestias, nihil expedita. Accusamus, sed quae!",
+    L3: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic aspernatur ipsum inventore ullam id tempora soluta quidem! Ut iure debitis architecto sunt, corrupti explicabo! Cum fugit error voluptatem corrupti vero.",
+}
+
+document.getElementById('L1').innerHTML = me.L1
+document.getElementById('L2').innerHTML = me.L2
+document.getElementById('L3').innerHTML = me.L3
+
+
+AOS.init();
