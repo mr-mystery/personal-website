2e25ba1ddf980eb6a022962a91900303f801ea79
Saket B <ken.life213@gmail.com>
2022-08-05 12:56:03 -0700

    Added Pages

---
+++ Home/main.js
@@ -0,0 +1,12 @@
+
+function link(num) {
+    let iframe
+    if(num==='one') {
+        iframe = document.getElementById('iframe')	
+        iframe.setAttribute('src', 'https://mr-mystery-13.github.io/hosting-demo/')
+    }
+    if(num==='two') {
+        iframe = document.getElementById('iframe')	
+        iframe.setAttribute('src', 'https://mr-mystery-13.github.io/Personal-Website/')
+    }
+}
