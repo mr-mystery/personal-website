2bf20240c92b75de652620b071936c0ae6ebf76f
Mr Mystery <105168056+Mr-Mystery-13@users.noreply.github.com>
2022-12-04 14:50:23 -0800

    APCSP Projects

--- main.js
+++ main.js
@@ -2,33 +2,51 @@ AOS.init();
 
 
 
+const iframe = document.getElementById('iframe');
+const aTag = document.getElementById('iframeLink');
 function link(num) {
-    let iframe = document.getElementById('iframe');
-    let aTag = document.getElementById('iframeLink');
     let iframeLink;
 
     if(num === 'one') {
-        iframeLink = 'https://mr-mystery.github.io/hosting-demo/'
+        iframeLink = 'https://mr-mystery.github.io/hosting-demo/';
     }
     else if(num === 'two') {
-        iframeLink = 'https://mr-mystery.github.io/Personal-Website/'
+        iframeLink = 'https://mr-mystery.github.io/Personal-Website/';
     }
     else if(num === 'three') {
-        iframeLink = 'https://mr-mystery.github.io/P5-Game/'
+        iframeLink = 'https://mr-mystery.github.io/P5-Game/';
     }
     else if(num === 'four') {
-        iframeLink = 'https://mr-mystery.github.io/firebase-messages/'
+        iframeLink = 'https://mr-mystery.github.io/firebase-messages/';
     }
     else if(num === 'five') {
-        iframeLink = 'https://mr-mystery.github.io/Random-Api/'
-    }
-    else if(num === 'APCSP') {
-        iframeLink = "https://codehs.com/sandbox/saketbhanvadia/yourusernamecodehsme/embed/?display_mode=displayOnly&read_only=True&show_file_tree=False"
-    }
+        iframeLink = 'https://mr-mystery.github.io/Random-Api/';
+    };
     
-    iframe.setAttribute('src', iframeLink)
-    aTag.setAttribute('href', iframeLink)
-}
+    iframe.setAttribute('src', iframeLink);
+    aTag.setAttribute('href', iframeLink);
+};
+function linkdrop(elem) {
+    let ev = elem.value;
+    let iframeLink;
+
+    if (ev === "APCSP") {
+        iframeLink = "https://codehs.com/sandbox/saketbhanvadia/yourusernamecodehsme/embed/?display_mode=displayOnly&read_only=True&show_file_tree=False";
+    }
+    else if (ev === "adventure") {
+        iframeLink = "https://trinket.io/embed/python/f92cf8aa76";
+    }
+    else if (ev === "cipher") {
+        iframeLink = "https://trinket.io/embed/python/7ba700f158";
+    }
+    else if (ev === "turtle") {
+        iframeLink = "https://trinket.io/embed/python/c3cef27e3d";
+    };
+
+    iframe.setAttribute('src', iframeLink);
+    aTag.setAttribute('href', iframeLink);
+    elem.value = ' ';
+};
 
 
 
@@ -43,23 +61,23 @@ function CheckBox() {
         LL1: "Super (More Info Coming Soon)",
         LL2: "Secret (More Info Coming Soon)",
         LL3: "Info (More Info Coming Soon)",
-    }
+    };
 
-    let label = document.getElementById('h1-label')
-    let li1 = document.getElementById('L1')
-    let li2 = document.getElementById('L2')
-    let li3 = document.getElementById('L3')
+    let label = document.getElementById('h1-label');
+    let li1 = document.getElementById('L1');
+    let li2 = document.getElementById('L2');
+    let li3 = document.getElementById('L3');
 
     if (document.getElementById('my-box').checked == true) {
-        label.innerHTML = AboutMe.label2
-        li1.innerHTML = AboutMe.LL1
-        li2.innerHTML = AboutMe.LL2
-        li3.innerHTML = AboutMe.LL3
+        label.innerHTML = AboutMe.label2;
+        li1.innerHTML = AboutMe.LL1;
+        li2.innerHTML = AboutMe.LL2;
+        li3.innerHTML = AboutMe.LL3;
     }
     else {
-        label.innerHTML = AboutMe.label1
-        li1.innerHTML = AboutMe.L1
-        li2.innerHTML = AboutMe.L2
-        li3.innerHTML = AboutMe.L3
-    }
-}
+        label.innerHTML = AboutMe.label1;
+        li1.innerHTML = AboutMe.L1;
+        li2.innerHTML = AboutMe.L2;
+        li3.innerHTML = AboutMe.L3;
+    };
+};
